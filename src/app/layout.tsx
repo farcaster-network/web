952291fa45074f3e7b170e35af9d149bab19cad0
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-05-04 23:19:36 -0700

    Always use dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,10 @@
 import type { Metadata } from "next";
-import { Inter, JetBrains_Mono } from "next/font/google";
+import { JetBrains_Mono } from "next/font/google";
 
 import { ThemeProvider } from "@/components/ThemeProvider";
 
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
 const mono = JetBrains_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -19,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="dark:bg-slate-950">
+    <html lang="en" className="bg-slate-950">
       <body className={mono.className}>
         <ThemeProvider
           attribute="class"
