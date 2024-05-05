6f5cab59bb73bc0edb519a7bfed0d536d6315335
Greg Skriloff <35093316+gskril@users.noreply.github.com>
2024-05-05 16:39:33 -0700

    Set twitter meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ export const metadata: Metadata = {
   openGraph: {
     images: ["/sharing.jpg"],
   },
+  twitter: {
+    card: "summary_large_image",
+  },
 };
 
 export default function RootLayout({
